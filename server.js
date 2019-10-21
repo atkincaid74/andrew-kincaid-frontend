4f5b124a90e92f17824438787c69a1f235de63d4
Andrew Kincaid <ATKincaid74@gmail.com>
2019-10-21 19:01:53 -0400

    trying out refresh bug fix

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// testing out refresh fix
+// https://stackoverflow.com/questions/44982580/vuewebpack-app-deploy-on-heroku-not-working-when-page-is-refreshed
+var history = require('connect-history-api-fallback');
+
 const express = require('express');
 const serveStatic = require("serve-static");
 const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history());
 const port = process.env.PORT || 80;
 app.listen(port);
