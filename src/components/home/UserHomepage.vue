dd874ecf570426a74755479c495eee13443066cd
Andrew Kincaid <ATKincaid74@gmail.com>
2019-10-29 19:49:11 -0400

    removed jojo table

--- src/components/home/UserHomepage.vue
+++ src/components/home/UserHomepage.vue
@@ -12,18 +12,6 @@
                 </v-col>
             </v-row>
         </v-container>
-        <v-container v-if="username === 'jojo69'">
-            <v-row justify="center">
-                <v-col cols="10">
-                    <v-data-table
-                        :headers="headersJojo"
-                        :items="dataJojo"
-                        disable-pagination
-                        hide-default-footer
-                    ></v-data-table>
-                </v-col>
-            </v-row>
-        </v-container>
     </div>
 </template>
 
@@ -57,58 +45,6 @@
                 {text: 'Last Name', value: 'last'},
                 {text: 'Paid', value: 'paid'},
             ],
-            headersJojo: [
-                {text: 'Joanne', value: 'jojo', align: 'center'},
-                {text: 'Category', value: 'cat', align: 'center'},
-                {text: 'Andrew', value: 'andy', align: 'center'},
-            ],
-            dataJojo: [
-                {
-                    jojo: 'Yes',
-                    cat: 'Prettiest ever?',
-                    andy: 'No',
-                },
-                {
-                    jojo: 'Yes',
-                    cat: 'As cute as it gets?',
-                    andy: 'No',
-                },
-                {
-                    jojo: 'Yes',
-                    cat: 'The most fun?',
-                    andy: 'No',
-                },
-                {
-                    jojo: 'Yes',
-                    cat: 'The sweetest?',
-                    andy: 'No',
-                },
-                {
-                    jojo: 'Yes',
-                    cat: 'Knows all of the Lil\' Jon?',
-                    andy: 'No',
-                },
-                {
-                    jojo: 'Yes',
-                    cat: 'Pro kickballer?',
-                    andy: 'No',
-                },
-                {
-                    jojo: 'Yes',
-                    cat: 'Has the cutest butt?',
-                    andy: 'No',
-                },
-                {
-                    jojo: '7',
-                    cat: 'Final Score',
-                    andy: '0',
-                },
-                {
-                    jojo: 'Yes',
-                    cat: 'Clearly the best ever?',
-                    andy: 'No',
-                },
-            ]
         }),
     }
 </script>
