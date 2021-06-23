02d4e7ba1a767bb34c17006f45cd4be78a4ad240
Andrew Kincaid <ATKincaid74@gmail.com>
2021-06-23 16:04:58 -0400

    added buttons for all forms

--- src/components/handicap/HandicapHome.vue
+++ src/components/handicap/HandicapHome.vue
@@ -3,6 +3,8 @@
     <v-card-title>Handicap</v-card-title>
     <v-card-actions>
       <v-btn @click="$router.push({name: 'CourseForm'})">Add Course</v-btn>
+      <v-btn @click="$router.push({name: 'ScorecardForm'})">Add Scorecard</v-btn>
+      <v-btn @click="$router.push({name: 'TeeForm'})">Add Tee</v-btn>
       <v-btn @click="$router.push({name: 'RoundForm'})">Add Round</v-btn>
       <v-btn @click="$router.push({name: 'HandicapHistory'})">Check Handicap</v-btn>
     </v-card-actions>
